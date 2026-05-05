d53d5ae1ee987df7f793cd21bdb2406ac762375e
Max29087 <topusorker087@gmai.com>
2026-05-05 20:49:21 +0600

    update 5 may 2026

---
+++ index.js
@@ -0,0 +1,46 @@
+let images = ["banner4.jpeg", "banner2.png", "banner3.png"];
+let index = 0;
+
+let banner = document.getElementById("banner");
+let slider = document.querySelector(".slider");
+
+let interval;
+
+function slide() {
+    let nextIndex = (index + 1) % images.length;
+
+    let img = new Image();
+    img.src = images[nextIndex];
+
+    banner.style.transform = "translateX(100%)";
+    banner.style.opacity = "0";
+
+    setTimeout(() => {
+        banner.src = images[nextIndex];
+
+        banner.style.transition = "none";
+        banner.style.transform = "translateX(-100%)";
+
+        banner.offsetHeight;
+
+        banner.style.transition = "transform 1s ease-in-out, opacity 1s ease-in-out";
+        banner.style.transform = "translateX(0)";
+        banner.style.opacity = "1";
+
+        index = nextIndex;
+    }, 1000);
+}
+
+function startSlider() {
+    interval = setInterval(slide, 6000);
+}
+
+function stopSlider() {
+    clearInterval(interval);
+}
+
+
+slider.addEventListener("mouseenter", stopSlider);
+slider.addEventListener("mouseleave", startSlider);
+
+startSlider();
